470fd3bbd102277371a8a84f3e244847864d4ba6
Ankush <ankushnathsharma50@gmail.com>
2026-01-01 22:51:06 +0530

    first commit

---
+++ ElectricitybillANS.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    int customerID, units;
+    float bill;
+
+    // Input customer details
+    printf("Enter Customer Name: ");
+    scanf("%s", name);
+
+    printf("Enter Customer ID: ");
+    scanf("%d", &customerID);
+
+    // Input units consumed
+    printf("Enter number of units consumed: ");
+    scanf("%d", &units);
+
+    // Calculate bill based on slabs
+    if (units <= 100) {
+        bill = units * 5.0;
+    } 
+    else if (units <= 200) {
+        bill = 100 * 5.0 + (units - 100) * 7.0;
+    } 
+    else {
+        bill = 100 * 5.0 + 100 * 7.0 + (units - 200) * 10.0;
+    }
+
+    // Print the bill
+    printf("\n----- Electricity Bill -----\n");
+    printf("Customer Name: %s\n", name);
+    printf("Customer ID: %d\n", customerID);
+    printf("Units Consumed: %d\n", units);
+    printf("Total Bill Amount: Rs. %.2f\n", bill);
+    printf("----------------------------\n");
+
+    return 0;
+}
